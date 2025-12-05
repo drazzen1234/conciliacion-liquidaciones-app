e23728672a4ffe50c5cc3e00cecaaac81d393ed1
drazzen1234 <drazzen@gmail.com>
2025-12-05 19:41:51 +0000

    Add app.py entry point

---
+++ app.py
@@ -0,0 +1,15 @@
+"""
+Entry point for production deployment
+"""
+import sys
+import os
+
+# Add the current directory to Python path
+sys.path.insert(0, os.path.dirname(__file__))
+
+# Import the Flask app from api.index
+from api.index import app
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
